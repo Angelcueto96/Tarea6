14721dc2d141b51f6161eb7d74c60afa424c8714
Angel Cueto <angelcueto96@outlook.com>
2018-10-16 23:19:50 -0500

    survey works

--- src/app/iphone-data.service.ts
+++ src/app/iphone-data.service.ts
@@ -11,13 +11,24 @@ export class IphoneDataService {
                 "name":"iphone XS",
                 "q1":"Do you have it or would you buy it?",
                 "q2":"Do you think the XS is the perfect size?",
-                "secondQuestions":{
-                    "true-true":"Do you consider it the best iPhone ever ?",
-                    "true-false":"Would you like it to be bigger or smaller",
-                    "false-true":"Would you buy it if it was cheaper?",
-                    "false-false":"Is this model really your favorite?"
-
-                }
+                "secondQuestions":[
+                    {
+                        "question":"Do you consider it the best iPhone ever ?",
+                        "description":"true-true"
+                    },
+                    {   
+                        "question":"Would you like it to be bigger or smaller",
+                        "description":"true-false"
+                    },
+                    {
+                        "question":"Would you buy it if it was cheaper?",
+                        "description":"false-true"
+                    },
+                    {
+                        "question":"Is this model really your favorite?",
+                        "description":"false-false"
+                    }
+                ]
 
 
             },
@@ -25,13 +36,25 @@ export class IphoneDataService {
                 "name":"iphone XS Max",
                 "q1":"Do you have it or would you buy it?",
                 "q2":"Do you think the XS Max is to big?",
-                "secondQuestions":{
-                    "true-true":"Do you consider it the best iPhone ever ?",
-                    "true-false":"Do you think it is the perfect size?",
-                    "false-true":"Do you think it is way too expensive?",
-                    "false-false":"Is this model really your favorite?"
-
-                }
+                "secondQuestions":[
+                    {
+                        "question":"Do you consider it the best iPhone ever ?",
+                        "description":"true-true"
+                    },
+                    {   
+                        "question":"Do you think it is the perfect size?",
+                        "description":"true-false"
+                    },
+                    {
+                        "question":"Do you think it is way too expensive?",
+                        "description":"false-true"
+                    },
+                    {
+                        "question":"Is this model really your favorite",
+                        "description":"false-false"
+                    }
+                ]
+                
 
 
             },
@@ -39,13 +62,25 @@ export class IphoneDataService {
                 "name":"iphone X",
                 "q1":"Do you like the borderless screen?",
                 "q2":"Do you think it is too expensive for what you get?",
-                "secondQuestions":{
-                    "true-true":" Would you buy it if it was cheaper?",
-                    "true-false":"Do you have it or would you buy it?",
-                    "false-true":"Is this model really your favorite?",
-                    "false-false":"Would you remove the borderless screen?"
-
-                }
+                "secondQuestions":[
+                    {
+                        "question":"Would you buy it if it was cheaper?",
+                        "description":"true-true"
+                    },
+                    {   
+                        "question":"Do you have it or would you buy it?",
+                        "description":"true-false"
+                    },
+                    {
+                        "question":"Is this model really your favorite?",
+                        "description":"false-true"
+                    },
+                    {
+                        "question":"Would you remove the borderless screen?",
+                        "description":"false-false"
+                    }
+                ]
+                
 
 
             },
@@ -53,13 +88,24 @@ export class IphoneDataService {
                 "name":"iphone 8",
                 "q1":"Do you feel like it is the same thing as an iphone X",
                 "q2":"Do you like the screen yo be bigger",
-                "secondQuestions":{
-                    "true-true": "Would you buy another iphone?",
-                    "true-false":"Do you think that it is the best iphone ever?",
-                    "false-true":"Would you rather have a better iphone",
-                    "false-false":"Do you think it is worth buying a better iphone?"
-
-                }
+                "secondQuestions":[
+                    {
+                        "question":"Would you buy another iphone?",
+                        "description":"true-true"
+                    },
+                    {   
+                        "question":"Do you think that it is the best iphone ever?",
+                        "description":"true-false"
+                    },
+                    {
+                        "question":"Would you rather have a better iphone",
+                        "description":"false-true"
+                    },
+                    {
+                        "question":"Do you think it is worth buying a better iphone?",
+                        "description":"false-false"
+                    }
+                ]
 
 
             },
@@ -67,13 +113,25 @@ export class IphoneDataService {
                 "name":"iphone 8 Plus",
                 "q1":"Do you think the plus size is the best size ever?",
                 "q2":"would you like it to have a borderless screen?",
-                "secondQuestions":{
-                    "true-true": "Would you buy a newer iphone?",
-                    "true-false":"Do you think this is the best iphone ever?",
-                    "false-true":"Would you buy a newer iphone if they were cheaper?",
-                    "false-false":"Would it be perfect if it was bigger?"
-
-                }
+                "secondQuestions":[
+                    {
+                        "question":"Would you buy a newer iphone?",
+                        "description":"true-true"
+                    },
+                    {   
+                        "question":"Do you think this is the best iphone ever?",
+                        "description":"true-false"
+                    },
+                    {
+                        "question":"Would you buy a newer iphone if they were cheaper?",
+                        "description":"false-true"
+                    },
+                    {
+                        "question":"Would it be perfect if it was bigger?",
+                        "description":"false-false"
+                    }
+                ]
+                
 
 
             }
@@ -82,7 +140,7 @@ export class IphoneDataService {
     }
 
     constructor() { }
-    
+
     getData():Object{
         return this.data;
     }
--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -10,7 +10,16 @@ import { FormControl } from '@angular/forms';
 export class SurveyComponent implements OnInit {
 
     data;
-    userSelection = 0; 
+    userFirstSelection = 0; 
+    userSecondSelection = 0;
+    userThirdSelection = 0;
+    question2Validation = false;
+    question3Validation = false;
+    question4Validation = false;
+
+    finalQuestion = 0;
+    userForthSelection = 0;
+
 
     constructor(private service:IphoneDataService) {
         this.data = this.service.getData();
@@ -21,13 +30,49 @@ export class SurveyComponent implements OnInit {
     }
 
     onChange(newValue) {
-        console.log(newValue);
-        this.userSelection = newValue;
 
-        let questions1 = document.getElementById('question2').style.visibility = "visible";
-        let questions2 = document.getElementById('question3').style.visibility = "visible";
+        this.userFirstSelection = newValue;
+
+        let question1 = document.getElementById('question2').style.visibility = "visible";
+        let question2 = document.getElementById('question3').style.visibility = "visible";
 
 
     }
+
+    onChangeQ2(newValue){
+        this.question2Validation = true;
+        console.log(newValue);
+        this.userSecondSelection = newValue;
+        this.validateThirdQuestion();
+    }
+    onChangeQ3(newValue){
+        this.question3Validation = true;
+        console.log(newValue);
+        this.userThirdSelection = newValue;
+        this.validateThirdQuestion();
+    }
+    validateThirdQuestion(){
+
+        if(this.question2Validation && this.question3Validation ){
+            let question4 = document.getElementById('question4').style.visibility = "visible";
+            if( this.userSecondSelection == 1 && this.userThirdSelection == 1){
+                this.finalQuestion = 0;
+            }else if(this.userSecondSelection == 1 && this.userThirdSelection == 0){
+                this.finalQuestion = 1;
+            }else if(this.userSecondSelection == 0 && this.userThirdSelection == 1){
+                this.finalQuestion = 2;
+            }else if(this.userSecondSelection == 0 && this.userThirdSelection == 0){
+                this.finalQuestion = 3;
+            }
+        }
+    }
+    onChangeQ4(newValue){
+        this.question4Validation = true;
+        console.log(newValue);
+        this.userForthSelection = newValue;
+        let submitButton = document.getElementById('submitButton').style.visibility = "visible";
+        
+    }
+
 
 }
